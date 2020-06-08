5db80217c830bd3465acc8cc88e3e53a0f3c21a4
VIGNESH4700 <60808963+VIGNESH4700@users.noreply.github.com>
2020-06-09 00:04:36 +0530

    Update App.js

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,21 +2,13 @@ import React, { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
+import { BrowserRouter, Route } from 'react-dom';
 
 function App() {
-  axios.get('https://localhost:5000/users/')
-        .then(response => {
-            const data = response.data;
-            for(let d of data)
-            {
-                    alert(d.username);
-            }
-        })
-        .catch(err => alert(err))
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
+    <BrowserRouter>
+      <Route exact path={"/"} component={HomeComponent} />
+    </BrowserRouter>
   );
 
 }
