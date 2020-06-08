433eef56b93f8963617fab7e9086e40af2d4b22a
VIGNESH4700 <60808963+VIGNESH4700@users.noreply.github.com>
2020-06-08 21:47:34 +0530

    Update App.js

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,8 +1,17 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  axios.get('http://localhost:5000/users/')
+        .then(response => {
+            const data = response.data;
+            for(let d of data)
+            {
+                    alert(d.username);
+            }
+  })
   return (
     <div className="App">
       <h1>Hello!</h1>
