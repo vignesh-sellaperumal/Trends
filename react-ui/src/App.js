42089142dc3ed591339af782fa19e3e9629587b3
VIGNESH4700 <60808963+VIGNESH4700@users.noreply.github.com>
2020-06-09 00:05:27 +0530

    Update App.js

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 import { BrowserRouter, Route } from 'react-dom';
+import HomeComponent from './pages/HomeComponent';
 
 function App() {
   return (
