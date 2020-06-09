58b0ae045d1ae2b000897503db8d2240a70d785a
VIGNESH4700 <60808963+VIGNESH4700@users.noreply.github.com>
2020-06-09 09:04:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express serve up index.html file if it doesn't recognize route
   
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'cd .. && react-ui', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, cd .., 'react-ui', 'build', 'index.html'));
   });
 }
 
